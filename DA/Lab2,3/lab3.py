fd2c325a4e69d3f7c8216a1537e4ee82117ab6d4
TheRegent <kobilinskiy.fedir@gmail.com>
2022-10-19 16:02:50 +0300

    start lab3

--- DA/Lab2,3/lab3.py
+++ DA/Lab2,3/lab3.py
@@ -1,10 +1,38 @@
+import pandas as pd
 #1. Виділити один зі стовпців (на вибір) з файлу як об’єкт Series, виділити з
 #нього підмасив. Задати назви індексів цього об’єкту. Виділити підмасиви
 #за допомогою прямої та непрямої індексацій.
+df = pd.read_csv('possum.csv', index_col=0)
+print(df.head())
 
+df_temp = df['totlngth']
+print(id(df_temp))
+print(id(df['totlngth']))
+
+df_temp = df.iloc[:, 4]
+print(id(df_temp))
+
+df_temp = df.loc[:, 'totlngth']
+print(id(df_temp))
+
+df_temp = df.iloc[1:5]
+print(id(df_temp))
+
+df_temp.index = ['a', 'b', 'c', 'd']
+print(df_temp)
+
+print(df_temp.loc[['a', 'd']])
 #2. До об’єкту DataFrame, в який записано вміст файлу, додати новий
 #стовпець, що є результатом операцій над іншими стовпцями. Також
 #продемонструвати додавання та видалення рядків, видалення стовпців.
+
+df['new'] = df['totlngth'] * df['age']
+df.head()
+
+df = df.append(df.loc[2])
+df.tail()
+
+df = df.drop(columns=['new'], index=[2])
 
 #3. Встановити один зі стовпців індексом. Визначити основні статистичні
 #характеристики та типи даних всіх стовпців. Змінити тип даних для
