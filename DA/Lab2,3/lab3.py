e856ffd1ca15186c64042e712c986372c671df3e
TheRegent <kobilinskiy.fedir@gmail.com>
2022-10-20 21:13:09 +0300

    developing lab3

--- DA/Lab2,3/lab3.py
+++ DA/Lab2,3/lab3.py
@@ -1,61 +1,78 @@
 import pandas as pd
+
+srs = pd.read_csv('possum.csv', index_col=0)
+print(srs.head())
 #1. Виділити один зі стовпців (на вибір) з файлу як об’єкт Series, виділити з
 #нього підмасив. Задати назви індексів цього об’єкту. Виділити підмасиви
 #за допомогою прямої та непрямої індексацій.
-df = pd.read_csv('possum.csv', index_col=0)
-print(df.head())
 
-df_temp = df['totlngth']
-print(id(df_temp))
-print(id(df['totlngth']))
+tmp = srs['totlngth']
+print(id(tmp))
+print(id(srs['totlngth']))
 
-df_temp = df.iloc[:, 4]
-print(id(df_temp))
+tmp = srs.iloc[:, 4]
+print(id(tmp))
 
-df_temp = df.loc[:, 'totlngth']
-print(id(df_temp))
+tmp = srs.loc[:, 'totlngth']
+print(id(tmp))
 
-df_temp = df.iloc[1:5]
-print(id(df_temp))
+tmp = srs.iloc[1:5]
+print(id(tmp))
 
-df_temp.index = ['a', 'b', 'c', 'd']
-print(df_temp)
+tmp.index = ['a', 'b', 'c', 'd']
+print(id(tmp))
 
-print(df_temp.loc[['a', 'd']])
+print(id(tmp.loc[['a', 'd']]))
+
 #2. До об’єкту DataFrame, в який записано вміст файлу, додати новий
 #стовпець, що є результатом операцій над іншими стовпцями. Також
 #продемонструвати додавання та видалення рядків, видалення стовпців.
 
-df['new'] = df['totlngth'] * df['age']
-print(df.head())
+srs['new'] = srs['totlngth'] * srs['age']
+print(srs.head())
 
-df = df.append(df.loc[2])
-print(df.tail())
+srs = srs.append(srs.loc[2])
+print(srs)
 
-df = df.drop(columns=['new'], index=[2])
-print(df)
+srs = srs.drop(columns=['new'], index=[2])
+print(srs)
 
 #3. Встановити один зі стовпців індексом. Визначити основні статистичні
 #характеристики та типи даних всіх стовпців. Змінити тип даних для
 #одного з стовпців. Згрупувати дані за одним зі стовпців, застосувати
 #кілька агрегуючих функцій, виділити підмасив за певними ознаками.
 
-df.set_index('pres.abs')
+srs.set_index('site')
 
-print(df.dtypes)
+print(srs.dtypes)
 
-print(df.describe())
+print(srs.describe())
 
-df['totlngth'] = df['totlngth'].astype('str')
-print(df.dtypes['totlngth'])
+srs['totlngth'] = srs['totlngth'].astype('str')
+print(srs.dtypes['totlngth'])
 
-print(df.groupby('pres.abs').agg({'chest':['mean', 'sum'], 'belly': 'std'}))
+print(srs.groupby('site').agg({'chest':['mean', 'sum'], 'belly': 'std'}))
 
-print(df[df['pres.abs'] == 0])
+print(srs[srs['site'] == 5])
 
 #4. Створити декілька власних об’єктів DataFrame за такою ж тематикою, що
 #й файл. Наприклад, якщо тема файлу – жаби, можна створити об’єкти,
 #що містять розміри жаб, вагу, стать, кількість особин в популяції і т.д.
 #Використати описані в теоретичних відомостях параметри методів merge
 #та concat для різних видів злиття та об’єднання даних цих об’єктів.
+
+types = pd.DataFrame({'possum-par': range(5),'color':['red', 'yellow', 'green', 'black', 'grey']})
+print(types)
+
+possum = pd.DataFrame({'name': ['Petro', 'Fedir', 'Ivan', 'Stepan', 'Andriy'], 'type': [3, 2, 4, 0, 1]})
+print(possum)
+
+new_possum = pd.DataFrame({'name': ['Lesya'], 'type':[4]})
+print(new_possum)
+
+possum = pd.concat([possum, new_possum], axis=0)
+print(possum)
+
+possum.merge(types, left_on='type', right_on='possum-par')
+
 
